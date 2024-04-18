9ffc4a228f7a768c014013984732c2e88e764e2e
Amansinh chauhan <amanchauhann551@gmail.com>
2024-04-18 09:28:54 +0530

    second file created

---
+++ problem1.java
@@ -0,0 +1,25 @@
+public class problem1 {
+    public static void main(String[] args) {
+    Solution solution = new Solution();
+    String a = "classrooms";
+    solution.solution(a);
+ }
+}
+
+class Solution {
+     public void solution(String a) {
+         int[] array = new int[26];
+         char[] char_array = a.toCharArray();
+
+         for(int i = 0; i < char_array.length; i++) {
+             int index = char_array[i] - 'a';
+             array[index]++;
+         }
+         int max = 0;
+         for(int i = 0; i < array.length; i++) {
+             if(array[i] > max) {
+                 max = array[i];
+             }
+         }
+     }
+ }
